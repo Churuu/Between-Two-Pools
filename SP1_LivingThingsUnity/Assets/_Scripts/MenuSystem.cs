044dad5fe046c19d0ad88cb3d584070e28b49f8f
jockek2 <joakim.karlsteen@gmail.com>
2019-02-08 10:41:19 +0100

    Påbörjat Menysystem

---
+++ SP1_LivingThingsUnity/Assets/_Scripts/MenuSystem.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour {
+
+    //Lägg till fler knappar om det behövs
+    public Button StartButton;
+    public Button ExitButton;
+    public Button Level;
+    public Button Restart;
+    public Button Back;
+    public Button Lvl1;
+    public Button BackToMainMenu;
+    public Canvas MainMenu;
+    public Canvas Levels;
+    public Canvas PauseMenu;
+    private bool P_Pressed;
+
+    void Start()
+    {
+        P_Pressed = false;
+        Time.timeScale = 1;
+        StartButton.OnSelect(null);
+        //Main Menu
+        StartButton.onClick.AddListener(TaskOnStart);
+        ExitButton.onClick.AddListener(TaskOnExitClick);
+        Level.onClick.AddListener(TaskOnLevel);
+        Back.onClick.AddListener(TaskOnBack);
+        //Pause menu
+        Restart.onClick.AddListener(TaskOnRestart);
+        BackToMainMenu.onClick.AddListener(TaskOnBackToMainMenu);
+    }
+    //Lägg till en knapp för varje level 
+    private void Update()
+    {   // visar pause menyn och Pausar också spelet och omvänt
+        if (Input.GetKeyDown(KeyCode.Escape) && P_Pressed == true)
+        {
+            PauseMenu.gameObject.SetActive(false);
+            P_Pressed = false;
+            Time.timeScale = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && P_Pressed == false)
+        {
+            PauseMenu.gameObject.SetActive(true);
+            Restart.Select();
+            P_Pressed = true;
+            Time.timeScale = 0;
+        }
+    }
+    void TaskOnStart()
+    {
+        SceneManager.LoadScene(/*Namn på scenen med lvl1*/" ");
+        MainMenu.gameObject.SetActive(false);
+    }
+    void TaskOnLevel()
+    {   
+        Levels.gameObject.SetActive(true);
+        MainMenu.gameObject.SetActive(false);
+        Back.Select();
+        Debug.Log("Select lvl");
+    }
+    void TaskOnExitClick()
+    {
+        //Stänger av spelet
+        Application.Quit();
+        Debug.Log("You have clicked the Exit button!");
+    }
+
+    void TaskOnBack()
+    {
+        Levels.gameObject.SetActive(false);
+        MainMenu.gameObject.SetActive(true);
+        StartButton.Select();
+    }
+    // Laddar level efter vilken knapp som tryckts
+    public void LoadLevel(string LevelSceneNAme)
+    {
+        SceneManager.LoadScene(LevelSceneNAme);
+    }
+
+    void TaskOnRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void TaskOnBackToMainMenu()
+    {
+        MainMenu.gameObject.SetActive(true);
+        PauseMenu.gameObject.SetActive(false);
+        P_Pressed = false;
+        StartButton.Select();
+    }
+}
