0759d3852443bde330799ac67d5627b95902a055
jockek2 <joakim.karlsteen@gmail.com>
2019-02-14 10:47:49 +0100

    Uppdaterad scenemanager

--- SP1_LivingThingsUnity/Assets/_Scripts/SceneManagerScript.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/SceneManagerScript.cs
@@ -2,21 +2,10 @@
 using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour {
-    private GameObject Seel;
-    private GameObject Frog;
-    private GameObject Otter;
 
 	public void LoadNextScene(string SceneName)
     {
-        Seel = GameObject.Find("Seel");
-        Frog = GameObject.Find("Frog");
-        Otter = GameObject.Find("Otter");
-        Scene CurrentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(SceneName);
-        /*SceneManager.MoveGameObjectToScene(Seel, SceneManager.GetSceneByName(SceneName));
-        SceneManager.MoveGameObjectToScene(Frog, SceneManager.GetSceneByName(SceneName));
-        SceneManager.MoveGameObjectToScene(karaktär3, SceneManager.GetSceneByName(SceneName));*/ // för att skicka med karaktärerna
-        SceneManager.UnloadSceneAsync(CurrentScene);
     }
 
     public void ReloadCurrentScene()
