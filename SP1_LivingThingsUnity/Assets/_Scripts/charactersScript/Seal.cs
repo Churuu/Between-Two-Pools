e856e6be1c6fe64ae8e8d17d22745b7fbe5c90f7
Robin Nechovski <a18robne@student.his.se>
2019-02-18 11:46:20 +0100

    tiles added test

---
+++ SP1_LivingThingsUnity/Assets/_Scripts/charactersScript/Seal.cs
@@ -0,0 +1,98 @@
+﻿//Skapad av Robin Nechovski 07-02-2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seal : MonoBehaviour
+{
+
+    [Range(50, 100)]
+    public float springBoostMultiplier;
+    [Range(0, 49)]
+    public float springBoostFallout;
+    public float BoostResetTimerDelta;
+    public float springJumpDistance;
+    public string bounceAnimParam;
+
+    [SerializeField] private string abilityKey;
+    private float boostResetTimer;
+    private bool jumped = false;
+    private bool shrunken = false;
+    private GameObject previousJumpingObject;
+    Animator anim;
+
+    void Start()
+    {
+        boostResetTimer = BoostResetTimerDelta;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        BoostLandingObject();
+        ResetBoostJump();
+        //Shrink();
+    }
+
+    void BoostLandingObject()
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, Vector2.up, springJumpDistance);
+
+        if (hit2D.collider != null && !shrunken)
+        {
+            if (hit2D.collider.gameObject != previousJumpingObject || previousJumpingObject == null)
+            {
+                jumped = false;
+                previousJumpingObject = hit2D.collider.gameObject;
+               // anim.ResetTrigger(bounceAnimParam);
+            }
+
+
+            if (hit2D.collider.GetComponent<Rigidbody2D>() != null)
+            {
+                var hitRB = hit2D.collider.GetComponent<Rigidbody2D>();
+
+                if (hitRB.velocity.y < 0)
+                {
+                    float velocityY = hitRB.velocity.y;
+                    hitRB.velocity = new Vector2(hitRB.velocity.x, 0);
+                    if (jumped)
+                    {
+                        springBoostMultiplier = springBoostFallout;
+                    }
+
+                    print(Mathf.Abs(hitRB.velocity.y) * springBoostMultiplier);
+
+                    hitRB.AddForce(Vector2.up * Mathf.Abs(velocityY) * springBoostMultiplier);
+                    boostResetTimer = Time.time + BoostResetTimerDelta;
+                    jumped = true;
+                    anim.SetTrigger(bounceAnimParam);
+                }
+            }
+        }
+    }
+
+    void ResetBoostJump()
+    {
+        if (Time.time > boostResetTimer)
+        {
+            jumped = false;
+            previousJumpingObject = null;
+            boostResetTimer = Time.time + BoostResetTimerDelta;
+            //anim.ResetTrigger(bounceAnimParam);
+        }
+    }
+
+    void Shrink()
+    {
+        if (Input.GetButtonDown(abilityKey))
+            shrunken = !shrunken;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, Vector3.up * springJumpDistance);
+    }
+}
