8ea75ac9189140268fff0ab66c4fddaeab627c9a
jockek2 <joakim.karlsteen@gmail.com>
2019-02-25 10:18:41 +0100

    Påbörjat deathscript

---
+++ SP1_LivingThingsUnity/Assets/_Scripts/ContactWithEnemy.cs
@@ -0,0 +1,33 @@
+﻿//Joakim
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactWithEnemy : MonoBehaviour {
+    public string enemyTag;
+    public string deadAnimParam;
+    private Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag(enemyTag))
+        {
+            if (GetComponent<Frog>() != null)
+                GetComponent<Frog>().enabled = false;
+           
+            if(GetComponent<Otter>() != null)
+                GetComponent<Otter>().enabled = false;
+
+            GetComponent<PlayerController>().enabled = false;
+            anim.SetBool(deadAnimParam, true);
+
+
+        }
+    }
+
+    
+}
