b6ce6894a4d57af71836969aa55837ed56ceb4ed
jockek2 <joakim.karlsteen@gmail.com>
2019-02-26 11:10:07 +0100

    Crushed scrip lagt på alla karaktärer

--- SP1_LivingThingsUnity/Assets/_Scripts/CrushedPlayer.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/CrushedPlayer.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class CrushedPlayer : MonoBehaviour {
     public float crushDistance = 1f;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] Collider2D[] crushingColliders;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -15,7 +16,7 @@ public class CrushedPlayer : MonoBehaviour {
         if (Crushed())
         {
             //Döda karaktären
-            Debug.Log("Player crushed");
+           
         }
         else { }
 	}
@@ -27,9 +28,29 @@ public class CrushedPlayer : MonoBehaviour {
         RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, crushDistance);
 
         if (hitDown.collider != null && hitUp.collider != null)
-            return true;
+            if(crushingColliders != null)
+                for(int i = 0; i < crushingColliders.Length; i++)
+                {
+                    for (int j = 0; j < crushingColliders.Length; j++)
+                    {
+                        if (hitDown.collider == crushingColliders[i] && hitUp.collider == crushingColliders[j])
+                        {
+                            Debug.Log("Player crushed");
+                            //Eventmanager.instance.onkilled(this.gameObject);
+                            return true;
+                        }
+                           
+                    }  
+                }
 
         return false;
 
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, Vector3.down * crushDistance);
+        Gizmos.DrawRay(transform.position, Vector3.up * crushDistance);
     }
 }
