f4d8e2b5ce4ac8ef5b94386a06967e11888a600b
jockek2 <joakim.karlsteen@gmail.com>
2019-03-04 14:24:16 +0100

    no message

--- SP1_LivingThingsUnity/Assets/_Scripts/Other misc/MagneticBoxAddToOtter.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/Other misc/MagneticBoxAddToOtter.cs
@@ -2,9 +2,15 @@
 
 public class MagneticBoxAddToOtter : MonoBehaviour
 {
+    [SerializeField] private float minDistanceStopMoving = 2.35f;
+    public GameObject[] characters;
     bool One = true;
+    private Rigidbody2D rb2D;
 
-
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +22,19 @@ public class MagneticBoxAddToOtter : MonoBehaviour
                 EventManager.instance.OnStartAddMagneticBox(gameObject.GetComponent<Rigidbody2D>());
             }
             One = false;
+        }
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if(characters[i] != null)
+            {
+                float Distance = Vector3.Distance(this.transform.position, characters[i].transform.position);
+
+                if (Distance < minDistanceStopMoving)
+                {
+                    Debug.Log("Nu ska den vara stilla");
+                    rb2D.velocity = new Vector2(0, 0);
+                }
+            }
         }
     }
 }
