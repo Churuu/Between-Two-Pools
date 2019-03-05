32f3d4300f9bd388ea40513004a8a7681f3b8767
Robin Nechovski <a18robne@student.his.se>
2019-03-05 11:53:14 +0100

    rockspawner

---
+++ SP1_LivingThingsUnity/Assets/_Scripts/Other misc/RockSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+
+    public GameObject rock;
+
+    GameObject _rock;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (_rock != null)
+            SpawnNewRock();
+    }
+
+
+    void SpawnNewRock()
+    {
+        _rock = Instantiate(rock, transform.position, transform.rotation) as GameObject;
+    }
+}
