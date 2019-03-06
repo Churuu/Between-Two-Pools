6200f5110b52adda5822930a5c2839172ac97fbf
Lillenooben <jesperli99@hotmail.com>
2019-03-06 15:09:21 +0100

    Fortsättning utav Stems osv, uppdaterat GameManagerObject prefaben för att även innehålla en child "StemsManager"

--- SP1_LivingThingsUnity/Assets/_Scripts/Other misc/StemsManager.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/Other misc/StemsManager.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class StemsManager : MonoBehaviour
 {
-    [SerializeField] AudioSource SealStemAudioSource;
+    [SerializeField] private AudioSource SealStemAudioSource;
 
-    [SerializeField] AudioSource OtterStemAudioSource;
+    [SerializeField] private AudioSource OtterStemAudioSource;
 
-    [SerializeField] AudioSource FrogStemAudioSource;
+    [SerializeField] private AudioSource FrogStemAudioSource;
 
+    private float SealStemVolume;
+    private float OtterStemVolume;
+    private float FrogStemVolume;
 
     // Use this for initialization
     void Start ()
     {
-		
+        SealStemVolume = SealStemAudioSource.volume;
+        OtterStemVolume = OtterStemAudioSource.volume;
+        FrogStemVolume = FrogStemAudioSource.volume;
 	}
 	
 	// Update is called once per frame
@@ -22,4 +27,23 @@ public class StemsManager : MonoBehaviour
     {
 		
 	}
+
+    public void ToSeal()
+    {
+        SealStemAudioSource.volume = SealStemVolume;
+        OtterStemAudioSource.volume = 0;
+        FrogStemAudioSource.volume = 0;
+    }
+    public void ToOtter()
+    {
+        SealStemAudioSource.volume = 0;
+        OtterStemAudioSource.volume = OtterStemVolume;
+        FrogStemAudioSource.volume = 0;
+    }
+    public void ToFrog()
+    {
+        SealStemAudioSource.volume = 0;
+        OtterStemAudioSource.volume = 0;
+        FrogStemAudioSource.volume = FrogStemVolume;
+    }
 }
