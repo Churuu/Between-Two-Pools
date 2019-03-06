879c7f6bd86860ee11757442303b529eb068112f
Lillenooben <jesperli99@hotmail.com>
2019-03-06 10:53:59 +0100

    avaktiverar audiosource tillsammans med sprite renderer

--- SP1_LivingThingsUnity/Assets/_Scripts/charactersScript/AnimManager.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/charactersScript/AnimManager.cs
@@ -6,12 +6,16 @@ public class AnimManager : MonoBehaviour
 {
 
     [SerializeField] SpriteRenderer redNarmal;
+    [SerializeField] AudioSource redNormalAudio;
 
     [SerializeField] SpriteRenderer redActiv;
+    [SerializeField] AudioSource redActiveAudio;
 
     [SerializeField] SpriteRenderer blueNarmal;
+    [SerializeField] AudioSource blueNormalAudio;
 
     [SerializeField] SpriteRenderer blueActiv;
+    [SerializeField] AudioSource blueActiveAudio;
 
     bool Pull;
     //  bool Thrust;
@@ -33,16 +37,24 @@ public class AnimManager : MonoBehaviour
             if (AbillityOn)
             {
                 redActiv.enabled = true;
+                redActiveAudio.enabled = true;
                 redNarmal.enabled = false;
+                redNormalAudio.enabled = false;
                 blueActiv.enabled = false;
+                blueActiveAudio.enabled = false;
                 blueNarmal.enabled = false;
+                blueNormalAudio.enabled = false;
             }
             else
             {
                 redNarmal.enabled = true;
+                redNormalAudio.enabled = true;
                 redActiv.enabled = false;
+                redActiveAudio.enabled = false;
                 blueActiv.enabled = false;
+                blueActiveAudio.enabled = false;
                 blueNarmal.enabled = false;
+                blueNormalAudio.enabled = false;
             }
         }
         else//Throst
@@ -50,17 +62,25 @@ public class AnimManager : MonoBehaviour
             if (AbillityOn)
             {
                 blueActiv.enabled = true;
+                blueActiveAudio.enabled = true;
                 redActiv.enabled = false;
+                redActiveAudio.enabled = false;
                 redNarmal.enabled = false;
+                redNormalAudio.enabled = false;
                 blueNarmal.enabled = false;
+                blueNormalAudio.enabled = false;
 
             }
             else
             {
                 blueNarmal.enabled = true;
+                blueNormalAudio.enabled = true;
                 redActiv.enabled = false;
+                redActiveAudio.enabled = false;
                 redNarmal.enabled = false;
+                redNormalAudio.enabled = false;
                 blueActiv.enabled = false;
+                blueActiveAudio.enabled = false;
             }
         }
     }
