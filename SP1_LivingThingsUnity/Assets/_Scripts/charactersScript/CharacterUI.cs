487612fac591658d85f9787dafb82e1d2dee640b
Robin Nechovski <a18robne@student.his.se>
2019-03-08 11:35:56 +0100

    bug fixes in characterUI

--- SP1_LivingThingsUnity/Assets/_Scripts/charactersScript/CharacterUI.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/charactersScript/CharacterUI.cs
@@ -21,7 +21,7 @@ public class CharacterUI : MonoBehaviour
     [Space]
     public Image selectedCharacterImage;
 
-    public enum states { dead, asleep, active, deadAndInactive }
+    public enum states { deadAndActive, inactive, active, deadAndInactive }
     public states otterState, frogState, sealState;
 
     OtterKilld otterKilld;
@@ -44,12 +44,20 @@ public class CharacterUI : MonoBehaviour
 
     void UpdateDeathState(GameObject obj)
     {
-        if (obj == otterKilld.gameObject)
-            otterState = states.dead;
-        if (obj == sealKilld.gameObject)
-            sealState = states.dead;
-        if (obj == frogKilld.gameObject)
-            frogState = states.dead;
+        if (obj == otterKilld.gameObject && otterState == states.inactive)
+            otterState = states.deadAndInactive;
+        else if (obj == otterKilld.gameObject && otterState == states.active)
+            otterState = states.deadAndActive;
+
+        if (obj == sealKilld.gameObject && sealState == states.inactive)
+            sealState = states.deadAndInactive;
+        else if (obj == sealKilld.gameObject && sealState == states.active)
+            sealState = states.deadAndActive;
+
+        if (obj == frogKilld.gameObject && frogState == states.inactive)
+            frogState = states.deadAndInactive;
+        else if (obj == frogKilld.gameObject && frogState == states.active)
+            frogState = states.deadAndActive;
     }
 
 
@@ -57,10 +65,10 @@ public class CharacterUI : MonoBehaviour
     {
         switch (otterState)
         {
-            case states.dead:
+            case states.deadAndActive:
                 otter.sprite = otterDead;
                 break;
-            case states.asleep:
+            case states.inactive:
                 otter.sprite = otterAsleep;
                 break;
             case states.active:
@@ -72,10 +80,10 @@ public class CharacterUI : MonoBehaviour
         }
         switch (frogState)
         {
-            case states.dead:
+            case states.deadAndActive:
                 frog.sprite = frogDead;
                 break;
-            case states.asleep:
+            case states.inactive:
                 frog.sprite = frogAsleep;
                 break;
             case states.active:
@@ -87,10 +95,10 @@ public class CharacterUI : MonoBehaviour
         }
         switch (sealState)
         {
-            case states.dead:
+            case states.deadAndActive:
                 seal.sprite = sealDead;
                 break;
-            case states.asleep:
+            case states.inactive:
                 seal.sprite = sealAsleep;
                 break;
             case states.active:
@@ -110,52 +118,52 @@ public class CharacterUI : MonoBehaviour
                 if (otterKilld.notKilld)
                     otterState = states.active;
                 else
-                    otterState = states.dead;
+                    otterState = states.deadAndActive;
 
                 if (sealKilld.notKilld)
-                    sealState = states.asleep;
+                    sealState = states.inactive;
                 else
                     sealState = states.deadAndInactive;
 
                 if (frogKilld.notKilld)
-                    frogState = states.asleep;
+                    frogState = states.inactive;
                 else
                     frogState = states.deadAndInactive;
 
-                    selectedCharacterImage.sprite = otterSelected;
+                selectedCharacterImage.sprite = otterSelected;
                 break;
             case 2:
                 if (otterKilld.notKilld)
-                    otterState = states.asleep;
+                    otterState = states.inactive;
                 else
                     otterState = states.deadAndInactive;
 
                 if (sealKilld.notKilld)
                     sealState = states.active;
                 else
-                    sealState = states.dead;
+                    sealState = states.deadAndActive;
 
                 if (frogKilld.notKilld)
-                    frogState = states.asleep;
+                    frogState = states.inactive;
                 else
                     frogState = states.deadAndInactive;
                 selectedCharacterImage.sprite = sealSelected;
                 break;
             case 3:
                 if (otterKilld.notKilld)
-                    otterState = states.asleep;
+                    otterState = states.inactive;
                 else
                     otterState = states.deadAndInactive;
 
                 if (sealKilld.notKilld)
-                    sealState = states.asleep;
+                    sealState = states.inactive;
                 else
                     sealState = states.deadAndInactive;
 
                 if (frogKilld.notKilld)
                     frogState = states.active;
                 else
-                    frogState = states.dead;
+                    frogState = states.deadAndActive;
                 selectedCharacterImage.sprite = frogSelected;
                 break;
         }
