c3944dc36904b974dd19ebccd528d5d56b0f89f5
Jonas Thunberg <thunberg.jonas95@gmail.com>
2019-03-12 10:15:01 +0100

    city chat scene

--- SP1_LivingThingsUnity/Assets/_Scripts/EventManagerScripts/EventChatScene.cs
+++ SP1_LivingThingsUnity/Assets/_Scripts/EventManagerScripts/EventChatScene.cs
@@ -9,6 +9,10 @@ public class EventChatScene : MonoBehaviour
     {
         EventManager.instance.OnChatActiv += NoMomentOnChat;
         EventManager.instance.OnChatEnd += ChatEndMomentOn;
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            gameObjects[i].GetComponent<PlayerController>().SetPlayerState(true);
+        }
     }
 
     // Update is called once per frame
@@ -21,14 +25,15 @@ public class EventChatScene : MonoBehaviour
     {
         for (int i = 0; i < gameObjects.Length; i++)
         {
-            gameObject.GetComponent<PlayerController>().SetPlayerState(false);
+            gameObjects[i].GetComponent<PlayerController>().SetPlayerState(false);
+            gameObjects[i].GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
         }
     }
     private void ChatEndMomentOn()
     {
         for (int i = 0; i < gameObjects.Length; i++)
         {
-            gameObject.GetComponent<PlayerController>().SetPlayerState(true);
+            gameObjects[i].GetComponent<PlayerController>().SetPlayerState(true);
         }
     }
 }
