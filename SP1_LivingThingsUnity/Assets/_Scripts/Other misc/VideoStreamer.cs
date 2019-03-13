b40ffc384a5d02e74efd2e4a83c53f59ba2d0ffb
Robin Nechovski <a18robne@student.his.se>
2019-03-13 11:29:38 +0100

    cutscenes inlagd

---
+++ SP1_LivingThingsUnity/Assets/_Scripts/Other misc/VideoStreamer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(AudioSource))]
+public class VideoStreamer : MonoBehaviour
+{
+    [Header("Playback image on canvas")]
+    public RawImage image;
+
+    VideoPlayer videoPlayer;
+    AudioSource source;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PrepareVideo()
+    {
+        videoPlayer.Prepare();
+    }
+
+    public void PlayVideo()
+    {
+        image.texture = videoPlayer.texture;
+        image.gameObject.SetActive(true);
+
+        videoPlayer.Play();
+        source.Play();
+
+    }
+
+    public bool isVideoPrepared()
+    {
+        return videoPlayer.isPrepared;
+    }
+
+    public float GetVideoLength()
+    {
+
+        float length = (float)videoPlayer.clip.length;
+        return length;
+    }
+}
