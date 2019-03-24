186fcc4bccc72ecb539a175d1a8bd18279d0453e
jockek2 <joakim.karlsteen@gmail.com>
2019-03-24 10:28:22 +0100

    Scrolling credits

---
+++ SP1_LivingThingsUnity/Assets/ScrollingCredits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Joakim
+public class ScrollingCredits : MonoBehaviour {
+
+    [SerializeField] float speed = 15f;
+
+        void Update()
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+}
